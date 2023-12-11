8bf1c0fe7d62d5ad26a05a35001abed6eac3779b
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2023-12-11 20:12:54 +0900

    add ログイン画面作ってみた

--- sample_svelte/src/routes/login/+page.svelte
+++ sample_svelte/src/routes/login/+page.svelte
@@ -31,15 +31,28 @@ async function handleLogin() {
         }
     } catch (error) {
         console.error('ログインエラー:', error);
+        alert('メールアドレスかパスワードが違います');
     }
 }
 </script>
 
-<form on:submit|preventDefault={handleLogin}>
-    <h1 class="text-3xl font-bold">ログイン</h1>
-    <label for='email'>メールアドレス</label>
-    <input id='email' type="email" bind:value={email} placeholder="Email">
-    <label for='password'>パスワード</label>
-    <input id='password' type="password" bind:value={password} placeholder="Password">
-    <button type="submit">ログイン</button>
-</form>
+<div class='relative flex-grow w-full max-w-xl mx-auto p-6 lg:p-8'>
+    <h1 class="text-3xl font-bold mb-6">ログイン</h1>
+    <form on:submit|preventDefault={handleLogin}>
+        <span class='flex items-center'>
+            <label for='email' class='text-sm font-bold leading-snug'>メールアドレス</label>
+        </span>
+        <div class='inline-flex w-full items-center bg-primary rounded cursor-text overflow-hidden relative mb-6'>
+            <input id='email' required type="email" bind:value={email} placeholder="hanako@example.com" class='border-gray-700 text-left appearance-none bg-transparent text-primary block flex-auto outline-none p-4 w-full bg-primary border border-primary rounded box-border text-base font-normal leading-snug m-0'>
+        </div>
+        <span class='flex items-center mb-1'>
+            <label for='password' class='text-sm font-bold leading-snug'>パスワード</label>
+        </span>
+        <div class='inline-flex w-full items-center bg-primary rounded cursor-text overflow-hidden relative'>
+            <input id='password' required type="password" bind:value={password} placeholder="1234abcd!" class='border-gray-700 text-left appearance-none bg-transparent text-primary block flex-auto outline-none p-4 w-full bg-primary border border-primary rounded box-border text-base font-normal leading-snug m-0'>
+        </div>
+        <div class='w-full h-11 text-base font-bold inline-flex items-center my-6'>
+            <button type="submit" disabled={!email || !password} class='bg-rose-600 text-white inline-flex items-center justify-center border border-transparent rounded px-4 py-3 w-full text-center transition duration-100 ease-in-out align-middle overflow-hidden whitespace-nowrap break-words'>ログイン</button>
+        </div>
+    </form>
+</div>
