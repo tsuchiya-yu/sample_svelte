dadafe0e5edc809ed2a2d14b3e57080b5663b479
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2023-12-31 00:29:56 +0900

    add フッター

---
+++ sample_svelte/src/lib/components/layout/footerGroup.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let title: string = '';
+    export let links: { title: string; url: string }[] = [];
+  </script>
+  
+  <div class='px-6 py-2.5'>
+    <div class='justify-between mb-20 mt-5 text-lg mb-4 font-bold'>{title}</div>
+    {#each links as link (link.title)}
+      <a href={link.url} class='block mb-2'>{link.title}</a>
+    {/each}
+  </div>
+  
--- sample_svelte/src/routes/+layout.svelte
+++ sample_svelte/src/routes/+layout.svelte
@@ -2,18 +2,25 @@
   import "../app.css";
   import { page } from '$app/stores';
   import { derived } from 'svelte/store';
+  import FooterGroup from '$lib/components/layout/footerGroup.svelte';
 
   // フッダーヘッターを表示しないパス
   const simpleLayoutPath = ['/login', '/signup'];
   const isSimpleLayout = derived(page, $page =>
     simpleLayoutPath.some(path => $page.url.pathname.startsWith(path))
   );
+
+  // フッターのグループ定義
+  let poricies = [
+    { title: 'プライバシーポリシー', url: '/privacy' },
+    { title: '利用規約', url: '/terms' }
+  ];
 </script>
 
 {#if !$isSimpleLayout}
-  <div class='bg-yellow-400 text-white mx-auto w-full'>
+  <div class='bg-yellow-400 text-white mx-auto w-full flex'>
     <div class='flex justify-between w-full mx-auto sm:w-[1100px]'>
-      <div>
+      <div class=''>
         <div class='flex justify-between'>ヘッダー</div>
       </div>
     </div>
@@ -23,11 +30,9 @@
 <slot />
 
 {#if !$isSimpleLayout}
-  <div class='bg-yellow-400 text-white mx-auto w-full py-20'>
-    <div class='flex justify-between w-full mx-auto sm:w-[1100px]'>
-      <div>
-        <div class='flex justify-between mb-20'>フッター</div>
-      </div>
+  <div class='bg-yellow-400 mx-auto w-full py-20'>
+    <div class='inline justify-start w-full mx-auto sm:w-[1100px] sm:flex'>
+      <FooterGroup title="プライバシーと利用規約" links={poricies} />
     </div>
   </div>
 {/if}
