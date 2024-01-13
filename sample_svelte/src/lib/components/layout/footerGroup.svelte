dc3cea219104b883b723683461b6fc2c87c4e108
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2024-01-13 11:10:52 +0900

    add 利用規約

--- sample_svelte/src/lib/components/layout/footerGroup.svelte
+++ sample_svelte/src/lib/components/layout/footerGroup.svelte
@@ -6,7 +6,7 @@
   <div class='px-6 py-2.5'>
     <div class='justify-between mb-20 mt-5 text-lg mb-4 font-bold'>{title}</div>
     {#each links as link (link.title)}
-      <a href={link.url} class='block mb-2'>{link.title}</a>
+      <a href={link.url} class='block mb-2 cursor-pointer'>{link.title}</a>
     {/each}
   </div>
   
