06220fd5d1d1444f90c1343718016e3704912b1e
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2024-02-18 21:55:42 +0900

    add プロフィール編集ページへの動線追加

--- sample_svelte/src/lib/components/ButtonReverse.svelte
+++ sample_svelte/src/lib/components/ButtonReverse.svelte
@@ -3,6 +3,7 @@
   export let text = '';
   export let disabled = false;
   export let type: 'button' | 'submit' | 'reset' = 'button'; // 型を明示的に指定
+  export let paddingClass = 'px-4 py-3'; // ボタン内の文字とボタンの枠線の間の余白を指定するクラス
 
   const dispatch = createEventDispatcher();
 
@@ -15,7 +16,7 @@
   <button
     type={type}
     disabled={disabled}
-    class={`bg-white text-yellow-400 inline-flex items-center justify-center border border-yellow-400 rounded px-4 py-3 w-full text-center transition duration-100 ease-in-out align-middle overflow-hidden whitespace-nowrap break-words outline-none ${!disabled ? '' : 'opacity-50'}`}
+    class={`bg-white text-yellow-400 inline-flex items-center justify-center border border-yellow-400 rounded ${paddingClass} w-full text-center transition duration-100 ease-in-out align-middle overflow-hidden whitespace-nowrap break-words outline-none ${!disabled ? '' : 'opacity-50'}`}
     on:click={handleClick}
   >
     {text}
--- sample_svelte/src/routes/mypage/+page.svelte
+++ sample_svelte/src/routes/mypage/+page.svelte
@@ -1,13 +1,43 @@
 <script lang="ts">
     import { onMount } from 'svelte';
-    import { checkLoginStatus } from '$lib/auth';
-    import Heading from '$lib/components/Heading.svelte';
+    import { goto } from '$app/navigation';
+    import { checkLoginStatus, currentUser } from '$lib/auth';
+    import type { User } from '../../types';
+    import Button from '$lib/components/Button.svelte';
+    import ButtonReverse from '$lib/components/ButtonReverse.svelte';
+
+    let user: User | null = null;
 
     onMount(async () => {
         await checkLoginStatus();
+        user = await currentUser();
+        console.log('user:', user);
     });
+
+    function gotoProfileEdit() {
+        goto('/mypage/profile');
+    }
+
+    function gotoProfile() {
+        if (user && user.id) {
+            goto(`/users/${user.id}/profile`);
+        }
+    }
 </script>
 
 <div class='relative flex-grow w-full max-w-xl mx-auto p-6 lg:p-8'>
-    <Heading text="マイページ" />
+    <div class="flex items-baseline">
+        <img class='w-32 h-32 object-contain m-auto' src={user?.userImageFile}/>
+        <div>
+            <h3 class='font-bold text-xl'>{user?.name ? user.name : '　'}</h3>
+            <div class='flex items-baseline'>
+                <div class='mr-3'>
+                    <Button text="プロフィール編集" paddingClass="px-2 py-1" on:click={gotoProfileEdit}/>
+                </div>
+                <div>
+                    <ButtonReverse text="プロフィールページ" paddingClass="px-2 py-1" on:click={gotoProfile}/>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
